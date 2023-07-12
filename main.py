b164f8df985cabc1eca23f0759185d28de7225c4
Replit user <>
2023-07-12 07:49:46 +0000

    zapp u sussy baka

--- main.py
+++ main.py
@@ -3,19 +3,13 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 import uvicorn
 
-app = FastAPI()
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 
 
-@app.get("/")
-async def root():
-    return "Ready"
-
-
-@app.get("/board")
-async def board(fen: str, flip: bool = False, before: str = None, after: str = None):
+def create(fen, flip, before, after):
     board = fenToImage(
         fen=f"{fen}",
-        squarelength=200,
+        squarelength=100,
         pieceSet=loadPiecesFolder("pieces"),
         darkColor="#79a65d",
         lightColor="#daf2cb",
@@ -30,7 +24,32 @@ async def board(fen: str, flip: bool = False, before: str = None, after: str = N
         else None,
     )
 
+    board = ImageOps.expand(board, border=40, fill="black")
+    font = ImageFont.truetype("font.ttf", 30)
+    Im = ImageDraw.Draw(board)
+    if not flip:
+        x = [chr(97 + i) for i in range(8)]
+        y = [str(8 - i) for i in range(8)]
+    else:
+        x = [chr(104 - i) for i in range(8)]
+        y = [str(1 + i) for i in range(8)]
+    for i in range(8):
+        Im.text((85 + i * 100, 840), x[i], (255, 255, 255), font=font)
+        Im.text((10, 75 + i * 100), y[i], (255, 255, 255), font=font)
     board.save("board.png")
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return "Ready"
+
+
+@app.get("/board")
+async def board(fen: str, flip: bool = False, before: str = None, after: str = None):
+    create(fen, flip, before, after)
     return FileResponse("board.png")
 
 
